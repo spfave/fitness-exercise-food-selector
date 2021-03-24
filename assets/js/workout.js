303944f33c4768a9040e3e46a2b95472511564e9
Sebastian F <sebpfave2@gmail.com>
2021-03-23 23:15:39 -0400

    Test wger api call with curl

---
+++ assets/js/workout.js
@@ -0,0 +1,8 @@
+//
+// Exercise categories: https://wger.de/api/v2/exercisecategory/
+// Exercise filtered by category: https://wger.de/api/v2/exercise/?category=10&language=2
+// Exercise info: https://wger.de/api/v2/exerciseinfo/345/
+// Exercise image: https://wger.de/api/v2/exerciseimage/345/thumbnails
+
+let workoutURL = "https://wger.de/api/v2/";
+workoutURL += `exercisecategory/?category=10`;
