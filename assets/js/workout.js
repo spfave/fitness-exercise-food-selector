f5da8c264ddaa0881a4f4b9ee2ce9554914b4e29
Sebastian F <sebpfave2@gmail.com>
2021-03-25 20:05:01 -0400

    Add comments

--- assets/js/workout.js
+++ assets/js/workout.js
@@ -4,29 +4,34 @@
 // Exercise info: https://wger.de/api/v2/exerciseinfo/345/
 // Exercise image: https://wger.de/api/v2/exerciseimage/345/thumbnails
 
-let workoutURL = "https://wger.de/api/v2/";
-workoutURL += `exercisecategory/?category=10`;
-
+// Call wger API for list of category exercises
 async function getExerciseList(exerciseCategoryID) {
+  // Create request url
   let url = `https://wger.de/api/v2/exercise/?category=${exerciseCategoryID}&language=2`;
 
+  // Handle bad response
   const response = await fetch(url);
   if (!response.ok) {
     throw response.json();
   }
 
+  // Return data if good resonse
   const data = await response.json();
   return data;
 }
 
+// Call wger API for details on exercise
 async function getExerciseInfo(exerciseID) {
+  // Create request url
   let url = `https://wger.de/api/v2/exerciseinfo/${exerciseID}`;
 
+  // Handle bad response
   const response = await fetch(url);
   if (!response.ok) {
     throw response.json();
   }
 
+  // Return data if good response
   const data = await response.json();
   return data;
 }
