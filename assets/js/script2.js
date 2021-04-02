92253c6fe566ac606777d8728280144e1be0f60a
Sebastian F <sebpfave2@gmail.com>
2021-04-02 19:44:38 -0400

    Food page changes

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,52 +1,48 @@
 // New_Branch
-document.getElementById("btn1").addEventListener('click', function (e) {
-    e.preventDefault();
-    // console.log("the btn has been clicked");
-    console.log(document.getElementById('exampleInputEmail1').value)
-    getRecipie(document.getElementById('exampleInputEmail1').value);
+document.getElementById("btn1").addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log("the btn has been clicked");
+  console.log(document.getElementById("exampleInputEmail1").value);
+  getRecipie(document.getElementById("exampleInputEmail1").value);
 });
 
 function getRecipie(userChoice) {
-    $.ajax({
-        url: "https://api.edamam.com/search?q="+userChoice+"&app_id=8e833b6c&app_key=a35a7f3bd00aa93c46dc3023dcf5866b" ,
-        method: "GET"
-    }).then(function (data) {
-        console.log(data)
+  $.ajax({
+    url:
+      "https://api.edamam.com/search?q=" +
+      userChoice +
+      "&app_id=8e833b6c&app_key=a35a7f3bd00aa93c46dc3023dcf5866b",
+    method: "GET",
+  }).then(function (data) {
+    console.log(data);
 
-        const url = data.hits[0].recipe.url
-        const imageurl = data.hits[0].recipe.image
+    const url = data.hits[0].recipe.url;
+    const imageurl = data.hits[0].recipe.image;
 
-      
-        var recipies = [];
+    var recipies = [];
 
-        for (let i = 0; i < data.hits.length; i++) {
-            const url = data.hits[i].recipe.url
-            const imageurl = data.hits[i].recipe.image
-            console.log("recipie");
-            
-            var recipieContent = `<a href="${url}" id="url">
+    for (let i = 0; i < data.hits.length; i++) {
+      const url = data.hits[i].recipe.url;
+      const imageurl = data.hits[i].recipe.image;
+      console.log("recipie");
+
+      var recipieContent = `<a href="${url}" id="url" target="_blank">
             <div class="card" style="width: 18rem">
             <div class="card-body">
                 <h5 id="recipie" class="card-Recipie"></h5>
             </div>
             <img id="image" src="${imageurl}" alt="" />
-            <button class="save-recipe"
+            <button class="save-recipe in"
                     id="center" style="color:white, background-color: #5e5e5e;">
                 Save Recipe
             </button>
             
             </div>
         </a>`;
-            
-            recipies.push(recipieContent)
-            
-          
-        
-        }
 
-        document.getElementById("recipeContainer").innerHTML = recipies;
-        
+      recipies.push(recipieContent);
+    }
 
-    })
+    document.getElementById("recipeContainer").innerHTML = recipies;
+  });
 }
-
